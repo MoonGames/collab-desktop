68d70bf381d585abf5ca5b6e4fc6bdc25e780eb4
Martin Indra <aktive@seznam.cz>
2013-02-10 01:29:27 +0100

    Switching version name from alpha 3 to alpha 4.

--- src/cz/mgn/collabdesktop/utils/CConstans.java
+++ src/cz/mgn/collabdesktop/utils/CConstans.java
@@ -28,5 +28,5 @@ public class CConstans {
 
     public static final String WEB_PAGE_MAIN = "http://collab.mgn.cz/";
     public static final String NAME = "Collab";
-    public static final String VERSION = "1.0 pre-alpha 3";
+    public static final String VERSION = "1.0 pre-alpha 4";
 }
